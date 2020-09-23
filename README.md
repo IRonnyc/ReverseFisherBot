5bfacf5c0caad9ffa35b9d1b0960d672e5e8157b
IRonnyc <nicolas.lagemann@gmail.com>
2020-09-23 16:40:40 +0200

    refactured emoji mapping to config.json

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # ReverseFisherBot
 
-add a config.json that contains your api token under the 'token' key:
-
+add a config.json that contains your api token under the 'token' key and then regular expressions and the emojis you want them to map to:
 ```
 {
-    "token": ""
+    "token": "",
+    "messageContainsMap": {
+        "bir(d|b)": ["🐦"],
+        "dragon": ["🐉"],
+        "sn(a|e)k(e)*": ["🐍"],
+        "cow": ["🐮"],
+        "m(u|o)+h{0,1}(\\W|\\b)": ["🐮"],
+        "fishes": ["🐠", "🐡", "🍥"],
+        ":o": ["😮"],
+        "D:": ["😦"],
+        "<3": ["♥"],
+        "\\o/": ["🎉"],
+        "y(a|e)+s": ["✔"],
+        "n(o)+(\\W|\\b)": ["❌"],
+        "nose": ["👃"]
+    }
 }
 ```
--- index.js
+++ index.js
@@ -7,30 +7,6 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const messageContainsMap = {
-    "bir(d|b)": ['🐦'],
-    "dragon": ['🐉'],
-    "ast(rologian){0,1}": ['💫'],
-    "dr(a)*g(oon){0,1}": ['☠'],
-    "heal": ['❤'],
-    "tank": ['🤩'],
-    "tonk": ['😍'],
-    "d(a)*m(a)*g(e)*": ['🤯'],
-    "d(e)*ps": ['🤯'],
-    "sn(a|e)k(e)*": ['🐍'],
-    "cow": ['🐮'],
-    "m(u|o)+h{0,1}(\\W|\\b)": ['🐮'],
-    "fishes": ['🐠', '🐡', '🍥'],
-    ":o": ['😮'],
-    "D:": ['😦'],
-    "<3": ['♥'],
-    ":v": ['🤪'],
-    "\\o/": ['🎉'],
-    "y(a|e)+s": ['✔'],
-    "n(o)+(\\W|\\b)": ['❌'],
-    "nose": ['👃'],
-}
-
 function addReactions(msg, reactions) {
     for (let i = 0; i < reactions.length; i++) {
         msg.react(reactions[i]);
@@ -40,7 +16,7 @@ function addReactions(msg, reactions) {
 client.on('message', msg => {
     msg.react('🐟');
     
-    for (const [key, reactions] of Object.entries(messageContainsMap)) {
+    for (const [key, reactions] of Object.entries(config.messageContainsMap)) {
         let keyRegex = new RegExp(key, 'i');
         if (keyRegex.test(msg)) {
             addReactions(msg, reactions);
