1d4fa5aaf2ba72579638ea14617e659f91a7f042
IRonnyc <nicolas.lagemann@gmail.com>
2020-09-24 19:41:06 +0200

    bot now deletes messages that trigger emotes, if possible

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function setRandomActivity() {
     let choice = statuses[Math.floor(Math.random() * statuses.length)];
     // set it!
     console.log(`changing activity to ${choice.name}`)
-    client.user.setActivity(choice.name, {type: choice.type});
+    client.user.setActivity(choice.name, { type: choice.type });
 }
 
 // when the client is ready
@@ -32,7 +32,7 @@ function addReactions(msg, reactions) {
     // iterate over reactions
     for (let i = 0; i < reactions.length; i++) {
         let emoji = reactions[i];
-        
+
         // if the text starts with 'c', it's supposed to use a custom emoji
         if (emoji.charAt(0) === 'c') {
             try {
@@ -48,15 +48,11 @@ function addReactions(msg, reactions) {
     }
 }
 
-// when a message is received
-client.on('message', msg => {
-    // add the 🐟 emoji
-    msg.react('🐟');
-
-    // first handle emotes
+// handles emotes and returns if the message should be looked at further (= has not been deleted).
+const handleEmotes = (msg) => {
     for (const [key, reactions] of Object.entries(config.emotes)) {
         if (msg.content.startsWith('/' + key)) {
-            
+
             let target = "";
             // values of msg.mentions.users as array
             let users = msg.mentions.users.array();
@@ -73,12 +69,12 @@ client.on('message', msg => {
             if (msg.mentions.everyone) {
                 target = "everyone"
             } else if (users.length > 0) { // otherwise build the list of mentioned users
-                
+
                 if (users.length > 1) {
                     // list all users except for the last and add an ", and " before the last
-                    target = users.slice(0, users.length -1).join(", ");
+                    target = users.slice(0, users.length - 1).join(", ");
                     // finish up the last part of target
-                    target = [target, users[users.length-1]].join(", and ");
+                    target = [target, users[users.length - 1]].join(", and ");
                 } else {
                     target = users[0];
                 }
@@ -87,10 +83,23 @@ client.on('message', msg => {
             let answer = target === "" ? reactions[0] : reactions[1];
 
             // send emote text
-            msg.channel.send(answer.replace("@author", msg.author.username).replace("@target", target));
+            msg.channel.send(answer.replace("@author", msg.author.username).replace("@target", target)).then(sent => {}).catch(console.error);
+
+            // if the bot has the permission to do so, delete the message triggering the emote
+            if (msg.channel.guild.me.permissions.has('MANAGE_MESSAGES')) {
+                msg.delete();
+                // message deleted, so no further processing of the message is possible
+                return false;
+            } else {
+                msg.react("💔");
+            }
         }
     }
+    return true;
+}
 
+// handles the wordMap and returns if the message should be looked at further
+const handleWordMap = (msg) => {
     // iterate over the wordMap
     for (const [key, reactions] of Object.entries(config.wordMap)) {
         // create a regex from the key
@@ -102,7 +111,11 @@ client.on('message', msg => {
             addReactions(msg, reactions);
         }
     }
+    return true;
+}
 
+// handles the userMap and returns if the message should be looked at further
+const handleUserMap = (msg) => {
     // iterate over the usernameMap
     for (const [key, reactions] of Object.entries(config.usernameMap)) {
         // create a regex from the key
@@ -113,6 +126,31 @@ client.on('message', msg => {
             // add reaction if the regex matches
             addReactions(msg, reactions);
         }
+    }
+    return true;
+}
+
+// when a message is received
+client.on('message', msg => {
+
+    // add the 🐟 emoji
+    msg.react('🐟');
+
+    // first handle emotes and check if the message processing should continue
+    if (msg.content.startsWith('/')) {
+        if (!handleEmotes(msg)) {
+            return;
+        }
+    }
+
+    // handle wordMap and check if the message processing should continue
+    if(!handleWordMap(msg)) {
+        return;
+    }
+
+    // handle userMap and check if the message processing should continue
+    if (!handleUserMap(msg)) {
+        return;
     }
 });
 
