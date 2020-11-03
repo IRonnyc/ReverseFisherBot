ce8387ad3dfb1b457d2e345e29028db61ceb1c81
IRonnyc <i@ironnyc.de>
2020-11-03 16:17:37 +0100

    sinyFormatter.js

---
+++ sinnyFormatter.js
@@ -0,0 +1,27 @@
+var fetch = require("node-fetch");
+
+const getFormattedString = (source, type, params, then) => {
+    let urlString = source + '?type=' + type + (params.length > 0 ? "&" : "") + params.join("&"); //param1=value&param2=value....
+
+    console.log("url: " + urlString);
+    console.log("source: " + source);
+    console.log("type: " + type);
+    console.log("params: " + params);
+    let returnValue = fetch(new  URL(urlString))
+        .then(response => {
+            if(!response.ok) {
+                return null;
+            }
+            return response.json();
+        })
+        .then(json => {
+            then(JSON.stringify(json));
+        }).catch(err => {
+            console.error(err);
+        });
+    return returnValue;
+}
+
+module.exports = {
+    getFormattedString
+}
